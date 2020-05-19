7fa7e840c03f7d3a13b1ccad3c092264c7e5e423
Romulo Mourão <romulo.dam@gmail.com>
2020-05-19 20:07:32 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Gitpr
+
+- This package open the github's comparison page between current branch and the target branch.
---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     name='gitpr',
+     version='0.1',
+     scripts=['gitpr'] ,
+     author="Romulo Mourão",
+     author_email="romulo.dam@gmail.com",
+     description="Github PR Utility",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/romulomourao/gitpr",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Development Status :: 3 - Alpha",
+     ],
+ )
